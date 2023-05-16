ae139b73a9a209cc0cf9874c6b72b56f0a673829
Matute1948 <achigfelipe@gmail.com>
2023-05-16 18:32:40 -0500

    Creacion profesor

--- src/main/java/com/example/pa2_u1_p4_af/Pa2U1P4AlApplication.java
+++ src/main/java/com/example/pa2_u1_p4_af/Pa2U1P4AlApplication.java
@@ -1,23 +1,30 @@
 package com.example.pa2_u1_p4_af;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Pa2U1P4AfApplication implements CommandLineRunner{
+public class Pa2U1P4AlApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
-		SpringApplication.run(Pa2U1P4AfApplication.class, args);
+		SpringApplication.run(Pa2U1P4AlApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
-		//spring framework: permite la construcciones/desarrola de apps empresariales 
-		//basadas en spring
+
+		Profesor profe = new Profesor();
+		profe.setNombre("Andres");
+		profe.setApellido("Lugmaña");
+		profe.setCedula("1724210685");
+		profe.setFecha(LocalDateTime.now());
 		
+		System.out.println(profe);
 	}
 
 }
